005a3e070a941447aca4f4a621ff35c30454de15
Corso <Corso@PC-037.homenet.telecomitalia.it>
2018-10-22 12:20:54 +0200

    Secondo Commit

--- src/caricaimmagine/MainCaricaImmagine.java
+++ src/caricaimmagine/MainCaricaImmagine.java
@@ -16,17 +16,25 @@ public class MainCaricaImmagine {
 //        chiudi quando schiacci sulla X
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         do {
+
+            String scelta;
+//            costruisco un dialogo con l'utente
+//            usando JFileChooser
+            JFileChooser fc = new JFileChooser();
+            int result = fc.showOpenDialog(null);
+//            aggiungiamo al frame la classe anonima CaricaImmagine
+//            alla quale passo scelta
+            if (result == JFileChooser.APPROVE_OPTION) {
+                scelta = fc.getSelectedFile().getName();
+                frame.add(new CaricaImmagine(scelta));
+            } else {
 //            costruisco un dialogo con l'utente
 //            usando il JOptionPane contenuto nella libreria swing
 //            acquisice una stringa tramite una finestra
-            String scelta = JOptionPane.showInputDialog(msg);
-//            costruisco un dialogo con l'utente
-//            usando JFileChooser
-//////            JFileChooser fc = new JFileChooser();
-//////            fc.showOpenDialog(null);
-//            aggiungiamo al frame la classe anonima CaricaImmagine
-//            alla quale passo scelta
-            frame.add(new CaricaImmagine(scelta));
+                scelta = JOptionPane.showInputDialog(msg);
+                frame.add(new CaricaImmagine(scelta + ".jpg"));
+            }
+
 //            aggiungiamo le componenti grafiche
             frame.pack();
 //            setto la visibilità a true
